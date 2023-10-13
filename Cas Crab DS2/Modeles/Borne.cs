97806f888f42a363c0609b5dbc2c47342ea6663d
Gorkeur <Alexandre.bar92fr@gmail.com>
2023-10-13 15:07:04 +0200

    Lien effectué entre Borne et TypeBorne

--- Cas Crab DS2/Modeles/Borne.cs
+++ Cas Crab DS2/Modeles/Borne.cs
@@ -14,15 +14,17 @@ namespace Cas_Crab_DS2.Modeles
         private int _IdBorne;
         private DateTime _DateDerniereRevesion;
         private int _IndiceCompteurUnites;
+        private TypeBorne _LeType;
 
         #endregion
 
         #region Constructeur
-        public Borne(int IdBorne, DateTime DateDerniereRevision, int IndiceCompteurUnites)
+        public Borne(int IdBorne, DateTime DateDerniereRevision, int IndiceCompteurUnites, TypeBorne LeType)
         {
             _IdBorne = IdBorne;
             _DateDerniereRevesion = DateDerniereRevision;
             _IndiceCompteurUnites = IndiceCompteurUnites;
+            _LeType = LeType;
 
             Borne.CollClasse.Add(this);
         }
@@ -30,6 +32,11 @@ namespace Cas_Crab_DS2.Modeles
         #endregion
 
         #region Getters/Setters
+        public int IdBorne { get => _IdBorne; set => _IdBorne = value; }
+        public DateTime DureeTotale { get => _DateDerniereRevesion; set => _DateDerniereRevesion = value; }
+        public int IndiceCompteurUnites { get => _IndiceCompteurUnites; set => _IndiceCompteurUnites =  value; }
+        public TypeBorne LeType { get => _LeType; set => _LeType = value; }
+
         #endregion
 
         #region Methodes
